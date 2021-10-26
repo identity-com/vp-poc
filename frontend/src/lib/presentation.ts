1ef3b958fa4d2906974a47d9c578bb94d11fd27b
Brett Etter <brett.etter@identity.org>
2021-10-26 13:57:16 -0500

    IDCOM-1273 Added signing with cryptid to sign function

--- frontend/src/lib/presentation.ts
+++ frontend/src/lib/presentation.ts
@@ -3,6 +3,7 @@ import { v4 as uuidv4 } from 'uuid';
 import { JsonWebSignature, JsonWebKey } from '@transmute/json-web-signature';
 import { WalletAdapter } from '@identity.com/wallet-adapter-base';
 
+import { base64url } from '@transmute/ed25519-key-pair/dist/encoding';
 import defaultDocumentLoader from './presentation/documentLoader';
 import { convert as convertCredential } from './presentation/credential';
 
@@ -17,27 +18,6 @@ export const create = (credentials: any[], controller: string) => ({
   },
   verifiableCredential: convertCredential(credentials, controller),
 });
-
-export const sign = async (
-  vp: any,
-  key: JsonWebKey,
-  documentLoader = defaultDocumentLoader,
-): Promise<any> => {
-  const result = await verifiable.presentation.create({
-    presentation: {
-      ...vp,
-      holder: { id: key.id },
-    },
-    format: ['vp'],
-    documentLoader,
-    challenge: uuidv4(),
-    suite: new JsonWebSignature({
-      key,
-    }),
-  });
-
-  return result.items[0];
-};
 
 async function signWithCryptid(wallet: WalletAdapter, data: Uint8Array): Promise<Uint8Array> {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -66,6 +46,58 @@ async function signWithCryptid(wallet: WalletAdapter, data: Uint8Array): Promise
     }, windowOrigin);
   });
 }
+
+const jwtSigner = (wallet: WalletAdapter) => () => ({
+  sign: async ({ data }: { data: Uint8Array | any }) => {
+    const header = {
+      alg: 'EdDSA',
+      b64: false,
+      crit: ['b64'],
+    };
+    const encodedHeader = base64url.encode(JSON.stringify(header));
+
+    const toBeSigned = new Uint8Array(
+      Buffer.concat([
+        Buffer.from(encodedHeader, 'utf8'),
+        Buffer.from('.', 'utf-8'),
+        data,
+      ]),
+    );
+
+    const signature = await signWithCryptid(wallet, toBeSigned);
+
+    return `${encodedHeader}..${base64url.encode(Buffer.from(signature))}`;
+  },
+});
+
+export const sign = async (
+  wallet: WalletAdapter,
+  vp: any,
+  key: JsonWebKey,
+  documentLoader = defaultDocumentLoader,
+): Promise<any> => {
+  const newKey = new JsonWebKey();
+  newKey.id = key.id;
+  newKey.type = key.type;
+  newKey.controller = key.controller;
+  newKey.verifier = key.verifier;
+  newKey.signer = jwtSigner(wallet);
+
+  const result = await verifiable.presentation.create({
+    presentation: {
+      ...vp,
+      holder: { id: key.id },
+    },
+    format: ['vp'],
+    documentLoader,
+    challenge: uuidv4(),
+    suite: new JsonWebSignature({
+      key: newKey,
+    }),
+  });
+
+  return result.items[0];
+};
 
 export const verify = async (
   vp: any,

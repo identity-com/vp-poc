b57f55f8651856cea92736f76a1b9197a38637a7
William Brooks <william@identity.org>
2021-10-29 16:35:42 +0200

    temporarily disabling tests

--- frontend/tests/unit/lib/presentation.spec.ts
+++ frontend/tests/unit/lib/presentation.spec.ts
@@ -1,102 +1,102 @@
-// import { JsonWebKey } from '@transmute/json-web-signature';
-// import { presentation } from '@/lib/index';
-// import credentials from './fixtures/credentials.json';
-// import { createJwkFromBs58 } from '@/lib/keyUtil';
-// import defaultDocumentLoader from '@/lib/presentation/documentLoader';
-// import didcontroller from './fixtures/didcontroller.json';
-//
-// const did = 'did:sol:devnet:3emPMNueBjcnLxpxJLrakNjBHyXZdZ1djdgqUvYNwpXF';
-// const keyBs58 = '22jH4D3nP2aELBvEMFHYd16MQNACy3zSKJTNj3aM2ic8nbkT9KEYEFMcg5XXr39KNe8GMFYefVAyfvEGLniZ884u';
-//
-// const createPresentation = () => presentation.create(credentials, did);
-// let jwk: JsonWebKey;
-//
-// // document loader to prevent reliance on solana node
-// const documentLoader = async (iri: string) => {
-//   if (iri.startsWith('did:sol')) {
-//     return {
-//       document: didcontroller,
-//     };
-//   }
-//
-//   return defaultDocumentLoader(iri);
-// };
-//
-// describe('Presentation Tests', () => {
-//   beforeAll(async () => {
-//     jwk = await createJwkFromBs58(keyBs58, did, 'default');
-//   });
-//
-//   it('creates a verifiable presentation', () => {
-//     const vp = createPresentation();
-//
-//     expect(vp)
-//       .toEqual(
-//         expect.objectContaining({
-//           type: ['VerifiablePresentation'],
-//           holder: { id: did },
-//           verifiableCredential: expect.arrayContaining([
-//             expect.objectContaining({
-//               type: ['VerifiableCredential', 'IdentityCredential'],
-//               credentialSubject: {
-//                 id: did,
-//               },
-//             }),
-//           ]),
-//         }),
-//       );
-//   });
-//
-//   it('signs a verifiable presentation', async () => {
-//     const vp = createPresentation();
-//
-//     const signedVp = await presentation.sign(jwk, vp);
-//
-//     expect(signedVp)
-//       .toEqual(
-//         expect.objectContaining({
-//           type: ['VerifiablePresentation'],
-//           holder: { id: `${did}#default` },
-//           verifiableCredential: expect.arrayContaining([
-//             expect.objectContaining({
-//               type: ['VerifiableCredential', 'IdentityCredential'],
-//               credentialSubject: {
-//                 id: did,
-//               },
-//             }),
-//           ]),
-//           proof: expect.objectContaining({
-//             type: 'JsonWebSignature2020',
-//             verificationMethod: `${did}#default`,
-//             proofPurpose: 'authentication',
-//             challenge: expect.any(String),
-//             jws: expect.any(String),
-//           }),
-//         }),
-//       );
-//   });
-//
-//   it('verifies a signed verifiable presentation', async () => {
-//     const vp = createPresentation();
-//
-//     const signedVp = await presentation.sign(jwk, vp);
-//
-//     const verified = await presentation.verify(signedVp);
-//
-//     expect(verified)
-//       .toEqual(true);
-//   });
-//
-//   it.only('fails to verify a tampered signed verifiable presentation', async () => {
-//     const vp = createPresentation();
-//
-//     const signedVp = await presentation.sign(jwk, vp);
-//
-//     // signedVp.proof.jws = 'eyJhbGciOiJFZERTQSIsImI2NCI6ZmFsc2UsImNyaXQiOlsiYjY0Il19..WKFBoCg4B-eAFROQCNtreY6WNz2WDjGjRl2M9nmSLwmIVnHyamYQ7ulh3FB6_l51uhC_RP19aGEk4LrPqGB0Cw';
-//
-//     signedVp.verifiableCredential[0].credentialSubject.id = '123';
-//     const verified = await presentation.verify(signedVp, documentLoader);
-//
-//     expect(verified).toEqual(false);
-//   });
-// });
+import { JsonWebKey } from '@transmute/json-web-signature';
+import { presentation } from '@/lib/index';
+import credentials from './fixtures/credentials.json';
+import { createJwkFromBs58 } from '@/lib/keyUtil';
+import defaultDocumentLoader from '@/lib/presentation/documentLoader';
+import didcontroller from './fixtures/didcontroller.json';
+
+const did = 'did:sol:devnet:3emPMNueBjcnLxpxJLrakNjBHyXZdZ1djdgqUvYNwpXF';
+const keyBs58 = '22jH4D3nP2aELBvEMFHYd16MQNACy3zSKJTNj3aM2ic8nbkT9KEYEFMcg5XXr39KNe8GMFYefVAyfvEGLniZ884u';
+
+const createPresentation = () => presentation.create(credentials, did);
+let jwk: JsonWebKey;
+
+// document loader to prevent reliance on solana node
+const documentLoader = async (iri: string) => {
+  if (iri.startsWith('did:sol')) {
+    return {
+      document: didcontroller,
+    };
+  }
+
+  return defaultDocumentLoader(iri);
+};
+
+describe('Presentation Tests', () => {
+  beforeAll(async () => {
+    jwk = await createJwkFromBs58(keyBs58, did, 'default');
+  });
+
+  it('creates a verifiable presentation', () => {
+    const vp = createPresentation();
+
+    expect(vp)
+      .toEqual(
+        expect.objectContaining({
+          type: ['VerifiablePresentation'],
+          holder: { id: did },
+          verifiableCredential: expect.arrayContaining([
+            expect.objectContaining({
+              type: ['VerifiableCredential', 'IdentityCredential'],
+              credentialSubject: {
+                id: did,
+              },
+            }),
+          ]),
+        }),
+      );
+  });
+
+  it('signs a verifiable presentation', async () => {
+    const vp = createPresentation();
+
+    const signedVp = await presentation.sign(jwk, vp);
+
+    expect(signedVp)
+      .toEqual(
+        expect.objectContaining({
+          type: ['VerifiablePresentation'],
+          holder: { id: `${did}#default` },
+          verifiableCredential: expect.arrayContaining([
+            expect.objectContaining({
+              type: ['VerifiableCredential', 'IdentityCredential'],
+              credentialSubject: {
+                id: did,
+              },
+            }),
+          ]),
+          proof: expect.objectContaining({
+            type: 'JsonWebSignature2020',
+            verificationMethod: `${did}#default`,
+            proofPurpose: 'authentication',
+            challenge: expect.any(String),
+            jws: expect.any(String),
+          }),
+        }),
+      );
+  });
+
+  it('verifies a signed verifiable presentation', async () => {
+    const vp = createPresentation();
+
+    const signedVp = await presentation.sign(jwk, vp);
+
+    const verified = await presentation.verify(signedVp);
+
+    expect(verified)
+      .toEqual(true);
+  });
+
+  it.only('fails to verify a tampered signed verifiable presentation', async () => {
+    const vp = createPresentation();
+
+    const signedVp = await presentation.sign(jwk, vp);
+
+    // signedVp.proof.jws = 'eyJhbGciOiJFZERTQSIsImI2NCI6ZmFsc2UsImNyaXQiOlsiYjY0Il19..WKFBoCg4B-eAFROQCNtreY6WNz2WDjGjRl2M9nmSLwmIVnHyamYQ7ulh3FB6_l51uhC_RP19aGEk4LrPqGB0Cw';
+
+    signedVp.verifiableCredential[0].credentialSubject.id = '123';
+    const verified = await presentation.verify(signedVp, documentLoader);
+
+    expect(verified).toEqual(false);
+  });
+});
